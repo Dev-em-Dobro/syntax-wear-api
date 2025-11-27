09a2b96400dd834e78da8716ab4bbb968fe4df34
Roberto Dias <level99devs@gmail.com>
2025-11-27 17:43:37 -0300

    feat: adiciona criação de produto

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -3,14 +3,13 @@ import { ProductFilters } from "../types";
 import { getProducts, getProductById } from "../services/products.service";
 import { productFiltersSchema } from "../utils/validators";
 
+export const listProducts = async (request: FastifyRequest<{ Querystring: ProductFilters }>, reply: FastifyReply) => {
+	const filters = productFiltersSchema.parse(request.query);
+	const result = await getProducts(filters as ProductFilters);
+	reply.status(200).send(result);
+};
 
-export const listProducts = async (request: FastifyRequest<{Querystring:ProductFilters}>, reply: FastifyReply) => {
-    const filters = productFiltersSchema.parse(request.query);
-    const result = await getProducts(filters as ProductFilters);
-    reply.status(200).send(result);
-}
-
-export const getProduct = async (request: FastifyRequest<{Params: {id: number}}>, reply: FastifyReply) => {
-    const product = await getProductById(request.params.id);
-    reply.status(200).send(product);
-}
+export const getProduct = async (request: FastifyRequest<{ Params: { id: number } }>, reply: FastifyReply) => {
+	const product = await getProductById(request.params.id);
+	reply.status(200).send(product);
+};
