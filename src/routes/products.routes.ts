f44d9bbf6e83fbb80938d53be232848a3b3c18e6
Roberto Dias <level99devs@gmail.com>
2025-11-28 12:05:53 -0300

    feat: cria funcionalidade de criação de produtos e busca de produto pelo id

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { getProduct, listProducts } from "../controllers/products.controller";
+import { createNewProduct, getProduct, listProducts } from "../controllers/products.controller";
 import { authenticate } from "../middlewares/auth.middleware";
+import { create } from "domain";
 
 export default async function productRoutes(fastify: FastifyInstance) {
-	fastify.addHook("onRequest", authenticate);
+	//fastify.addHook("onRequest", authenticate);
 	fastify.get(
 		"/",
 		{
@@ -12,36 +13,36 @@ export default async function productRoutes(fastify: FastifyInstance) {
 				description: "Lista produtos com filtros opcionais",
 				response: {
 					200: {
-						description: 'Lista de produtos',
-						type: 'array',
+						description: "Lista de produtos",
+						type: "array",
 						items: {
-							type: 'object',
+							type: "object",
 							properties: {
-								id: { type: 'string' },
-								name: { type: 'string' },
-								description: { type: 'string' },
-								price: { type: 'number' },
+								id: { type: "string" },
+								name: { type: "string" },
+								description: { type: "string" },
+								price: { type: "number" },
 								colors: {
-									type: 'array',
-									items: { type: 'string' }
+									type: "array",
+									items: { type: "string" },
 								},
-								createdAt: { type: 'string', format: 'date-time' },
-								updatedAt: { type: 'string', format: 'date-time' },
+								createdAt: { type: "string", format: "date-time" },
+								updatedAt: { type: "string", format: "date-time" },
 							},
 						},
 					},
 					400: {
-						description: 'Requisição inválida',
-						type: 'object',
+						description: "Requisição inválida",
+						type: "object",
 						properties: {
-							message: { type: 'string' },
+							message: { type: "string" },
 						},
 					},
 					401: {
-						description: 'Não autorizado',
-						type: 'object',
+						description: "Não autorizado",
+						type: "object",
 						properties: {
-							message: { type: 'string' },
+							message: { type: "string" },
 						},
 					},
 				},
@@ -104,23 +105,53 @@ export default async function productRoutes(fastify: FastifyInstance) {
 							updatedAt: { type: "string", format: "date-time" },
 						},
 					},
-                    400: {
-                        description: "Requisição inválida",
-                        type: "object",
-                        properties: {
-                            message: { type: "string" },    
-                        }
-                    },
-                    401: {
-                        description: "Não autorizado",
-                        type: "object",
-                        properties: {
-                            message: { type: "string" },
-                        }
-                    }
+					400: {
+						description: "Requisição inválida",
+						type: "object",
+						properties: {
+							message: { type: "string" },
+						},
+					},
+					401: {
+						description: "Não autorizado",
+						type: "object",
+						properties: {
+							message: { type: "string" },
+						},
+					},
 				},
 			},
 		},
 		getProduct
 	);
+
+	fastify.post("/", {
+		schema: {
+			tags: ["Products"],
+			description: "Criar um novo produto",
+			required: ["name", "description", "price", "slug", "active", "stock"],
+			body: {
+				type: "object",
+				properties: {
+					name: { type: "string" },
+					description: { type: "string" },
+					price: { type: "number" },
+					active: { type: "boolean" },
+					stock: { type: "number" },
+					colors: {
+						type: "array",
+						items: { type: "string" },
+					},
+					images: {
+						type: "array",
+						items: { type: "string" },
+					},
+					sizes: {
+						type: "array",
+						items: { type: "string" },
+					},
+				},
+			},
+		},
+	}, createNewProduct);
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../utils/prisma";
-import { ProductFilters } from "../types";
+import { CreateProduct, ProductFilters } from "../types";
 
 export const getProducts = async (filter: ProductFilters) => {
 	const { minPrice, maxPrice, search, sortBy, sortOrder, page = 1, limit = 10 } = filter;
@@ -81,3 +81,17 @@ export const getProductById = async (id: number) => {
 
 	return product;
 };
+
+export const createProduct = async (data: CreateProduct) => {
+
+	const existingProduct = await prisma.product.findUnique({
+		where: { slug: data.slug },
+	});
+
+	if(existingProduct){
+		throw new Error("Slug já existe. Escolha outro nome para o produto.");
+	}
+
+	const newProduct = await prisma.product.create({data});
+	return newProduct;
+}
