9ec93d2991bf498951f449ed25a54def3feb0f4f
Roberto Dias <level99devs@gmail.com>
2025-12-01 17:56:51 -0300

    feat: adiciona funcionalidade de deleção de categoria e atualização

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { CategoryFilters, CreateCategory } from "../types";
-import { getCategories, getCategoryById, createCategory } from "../services/categories.service";
-import { categoryFiltersSchema, createCategorySchema } from "../utils/validators";
+import { CategoryFilters, CreateCategory, UpdateCategory } from "../types";
+import { getCategories, getCategoryById, createCategory, updateCategory, deleteCategory } from "../services/categories.service";
+import { categoryFiltersSchema, createCategorySchema, updateCategorySchema } from "../utils/validators";
 import slugify from "slugify";
 
 export const listCategories = async (request: FastifyRequest<{ Querystring: CategoryFilters }>, reply: FastifyReply) => {
@@ -28,4 +28,28 @@ export const createNewCategory = async (request: FastifyRequest<{ Body: CreateCa
 	await createCategory(validate);
 
 	reply.status(201).send({ message: "Categoria criada com sucesso" });
+};
+
+export const updateExistingCategory = async (request: FastifyRequest<{ Params: { id: string }; Body: Partial<CreateCategory> }>, reply: FastifyReply) => {
+	const { id } = request.params;
+	const body = request.body;
+
+	const validate = updateCategorySchema.parse(body);
+
+	if (validate.name) {
+		validate.slug = slugify(validate.name, {
+			lower: true,
+			strict: true,
+			locale: "pt",
+		});
+	}
+
+	const category = await updateCategory(Number(id), validate);
+	reply.status(200).send(category);
+};
+
+export const deleteExistingCategory = async (request: FastifyRequest<{ Params: { id: number } }>, reply: FastifyReply) => {
+	const { id } = request.params;
+	await deleteCategory(id);
+	reply.status(204).send();
 };
