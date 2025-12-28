2ae2280acd4decb8eb65d02c0f3682d2a0fa916b
Ricardo Dias <cadu.hd@gmail.com>
2025-12-28 16:35:58 -0300

    feat: melhorias no método de login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,31 +3,48 @@ import { loginUser, registerUser } from "../services/auth.service";
 import { AuthRequest, RegisterRequest } from "../types";
 import { loginSchema, registerSchema } from "../utils/validators";
 
-export const register = async (request: FastifyRequest, reply: FastifyReply) => {
-	// Lógica de registro de usuário
+export const register = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  // Lógica de registro de usuário
 
-    const validation = registerSchema.parse(request.body as RegisterRequest);
+  const validation = registerSchema.parse(request.body as RegisterRequest);
 
-	const user = await registerUser(validation);
+  const user = await registerUser(validation);
 
-	const token = request.server.jwt.sign({ userId: user.id });
+  const token = request.server.jwt.sign({ userId: user.id });
 
-	reply.status(201).send({
-		user,
-		token,
-	});
+  reply.status(201).send({
+    user,
+    token,
+  });
 };
 
-export const login = async (request: FastifyRequest<{ Body: AuthRequest }>, reply: FastifyReply) => {
+export const login = async (
+  request: FastifyRequest<{ Body: AuthRequest }>,
+  reply: FastifyReply
+) => {
+  const validation = loginSchema.parse(request.body as AuthRequest);
 
-	const validation = loginSchema.parse(request.body as AuthRequest);
+  const user = await loginUser(validation, reply);
 
-	const user = await loginUser(validation);
+  if (!user) return;
 
-	const token = request.server.jwt.sign({ userId: user.id });
+  const token = request.server.jwt.sign({ userId: user.id });
 
-	reply.status(200).send({
-		user,
-		token,
-	});
+  reply.setCookie("syntaxwear.token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24,
+  });
+
+  reply.status(200).send({
+    user,
+  });
 };
+
+export const profile = async (request: FastifyRequest, reply: FastifyReply) =>
+  reply.send(request.user);
